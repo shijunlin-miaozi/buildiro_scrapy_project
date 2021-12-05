06755ed0b1ed967d7bf2f1d482510f6f12dff571
shijunlin <miaozilsj@gmail.com>
2021-12-05 09:37:51 +0100

    update

--- lightscraper/lightscraper/spiders/lightspider.py
+++ lightscraper/lightscraper/spiders/lightspider.py
@@ -7,12 +7,10 @@ from selenium.webdriver.common.by import By
 
 class LightSpider(scrapy.Spider):
     name = 'lights_co_uk'
-    # start_urls = ['https://www.lights.co.uk/philips-hue-whitel-color-impress-led-pillar-light.html'] # 404 error - scraper blocked
-    start_urls = ['https://www.lights.co.uk/philips-hue-white-color-impress-led-path-light.html']
-    # start_urls = ['https://www.lights.co.uk/philips-hue-white-color-econic-led-path-light.html']
-    # start_urls = ['https://www.lights.co.uk/philips-hue-waca-resonate-outdoor-wall-lamp-black.html'] # no video
+    # start_urls = ['https://www.lights.co.uk/philips-hue-whitel-color-impress-led-pillar-light.html'] # scraper blocked, cannot test result
+    start_urls = ['https://www.lights.co.uk/philips-hue-white-color-impress-led-path-light.html'] # video & pdf
+    # start_urls = ['https://www.lights.co.uk/klemens-path-light-with-motion-detector-65-cm.html'] # no video
     # start_urls = ['https://www.lights.co.uk/eve-flare-smart-led-light-for-indoors-and-out.html'] # no video, no pdf
-
 
     def parse(self, response):
         file_urls, image_urls = [], []
@@ -21,16 +19,17 @@ class LightSpider(scrapy.Spider):
         s = Service(settings.get('CHROME_DRIVER_PATH'))
         driver = Chrome(service = s)
         driver.get(response.url)
-        driver.implicitly_wait(1)
 
-        has_pdf = response.xpath('//a[@class="download-file__link"]')
         try: 
-            has_video = driver.find_element(By.TAG_NAME, 'video')
-        except:
-            has_video = []
-
-        if has_video:
             video = driver.find_element(By.TAG_NAME, 'video')
+        except:
+            video = []
+        try:
+            pdf = driver.find_element(By.CLASS_NAME, 'download-file__link')
+        except:
+            pdf = []
+
+        if video:
             play_btn = driver.find_element(By.XPATH, '//*[@id="controls"]/div/div/div[1]')
             pause_btn = driver.find_element(By.XPATH, '//*[@id="controls"]/div/div/div[2]')
             play_btn.click()
@@ -38,8 +37,7 @@ class LightSpider(scrapy.Spider):
             pause_btn.click()
             file_urls.append(video.get_attribute('src'))
 
-        if has_pdf:
-            pdf = driver.find_element(By.CLASS_NAME, 'download-file__link')
+        if pdf:
             pdf.click()
             file_urls.append(pdf.get_attribute('href'))
 
